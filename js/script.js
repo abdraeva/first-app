453310f486d7ccfec6a233d63ecd5d91b98270b9
Guldana <g.abdyjalieva@gmail.com>
2022-11-22 15:35:25 +0300

    Hello world

---
+++ js/script.js
@@ -0,0 +1,104 @@
+// for(START, CONDITION, STEP){
+// .. code
+// }
+
+// for(var i = 0; 1 < 10; i++){
+//     console.log(i)
+// }
+
+// 1) i = 10;
+// 2) i < 10;
+// 4) i = i + i
+
+
+// 3) result: 0
+// 3) result: 1
+
+
+// for(var i = 0; i < 20; i++){
+//     if( i % 2 == 0){
+//         console.log(i)
+//     }
+// }
+
+// for(var i = 0; i < 20; i++){
+//     console.log("Our services" + i)
+// }
+
+// for (var i = 0; i < 30; i++){
+//     if( i % 2 == 1){
+//         console.log(i)
+//     }
+// }
+
+// for(var i = 0; i < 20; i++){
+//     if(i ===  9){
+//         break
+//     }else{
+//         console,log(i)
+//     }
+// }
+
+// for(var i = 0; i < 20; i++){
+//     if(i === 2 || i = 10 || i === 13){
+//         continue
+//     }else{
+//         console,log(i)
+//     }
+// }
+
+
+
+// var start = +prompt("Start")
+// var condition = +prompt("Condition")
+// var step = +prompt("Step")
+
+// for(i = start; i < condition; i+= step){
+//     console.log(i)
+// }
+
+// var template = '<div class="card>Hello</div>';
+// for(var i = 0; i < 20; i++){
+//     document.write(template)
+// }
+
+// var askFriend = +prompt("How many friend do you have?")
+// var myFriend = "";
+// for(var i = 0; i< askFriend; i++){
+//     var friends = prompt ("The name of your" + (i + 1) + "friend");
+//     myFriend += friends + " ";
+
+// }
+
+// console.log(myFriend)
+
+
+// hometask
+
+// for(i = 1; i <= 15; i++){
+//     console.log(i)
+// }
+
+// var askAnimal = +prompt("How many animals do you have?")
+var theNamesOfAnimal = " ";
+for(i = 1; i <= 3; i++){
+    var animals = prompt("The name of your animals " + i);
+    theNamesOfAnimal += animals + " ";  
+    if(askAnimal === (3){
+        for(i = 1; i <= 15; i++){
+            console.log(i)
+        }
+    
+    }
+    
+    
+
+}
+
+console.log(theNamesOfAnimal) 
+
+
+// for(i = -908; i <= 67; i++){
+//     console.log(Math.abs(i))
+// }
+
